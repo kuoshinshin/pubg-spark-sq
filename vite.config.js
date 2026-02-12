0aaeecfa5bb13692a7a89bc6f24e8870bf18973d
kuoshinshin <kuoshinshin@example.com>
2026-02-12 15:48:50 +0800

    更新项目配置

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,6 @@ export default defineConfig({
           // 将大型组件分离到单独的代码块
           'chat': ['./src/components/chat/Chat.vue'],
           'match': ['./src/components/match/Match.vue'],
-          'event': ['./src/components/event/Event.vue'],
           'profile': ['./src/components/profile/Profile.vue']
         }
       }
